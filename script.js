5c669e14cb71afd8f75dc09d2951268db5ce2656
Sachinrawat1215 <sachinrawat0853@gmail.com>
2021-10-14 16:17:39 +0530

    Body height and margin top issue fixed...

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ console.log('This is my portfolio')
 // Declaring all the variables
 let navBtn = document.getElementById('nav_icon');
 let navBox = document.getElementById('nav_container');
+let container = document.getElementById("container");
+console.log(window.innerWidth);
 
-function controlPage(){
+function controlPage() {
     // Removing all active class
     about.classList.remove('active')
     portfolio.classList.remove('active')
@@ -18,30 +20,59 @@ function controlPage(){
     resume_container.style.display = "none";
 }
 
-navBtn.addEventListener('click',() => {
+navBtn.addEventListener('click', () => {
     navBox.classList.toggle('active');
-    resume.addEventListener('click',() => {
+    resume.addEventListener('click', () => {
         controlPage();
         resume.classList.add('active');
         resume_container.style.display = "block";
-    })
+        if (window.innerWidth <= 870) {
+            document.body.style.height = "1450px";
+            container.style.marginTop = "-670px";
+        }
+        if (window.innerWidth <= 560) {
+            document.body.style.height = "1910px";
+            container.style.marginTop = "-1160px";
+        }
+    });
 
-    portfolio.addEventListener('click',() => {
+    portfolio.addEventListener('click', () => {
         controlPage();
         portfolio.classList.add('active');
         portfolio_container.style.display = "block";
-    })
+        if (window.innerWidth <= 870 && window.innerWidth >= 561) {
+            document.body.style.height = "1600px";
+            container.style.marginTop = "-800px";
+        }else if (window.innerWidth <= 560) {
+            document.body.style.height = "2000px";
+            container.style.marginTop = "-1210px";
+            console.log("this is first working...")
+        };
+    });
 
-    contact.addEventListener('click',() => {
+    contact.addEventListener('click', () => {
         controlPage();
         contact.classList.add('active')
         contact_container.style.display = "block";
-    })
+        if (window.innerWidth <= 870) {
+            document.body.style.height = "1450px";
+            container.style.marginTop = "-670px";
+        }
+    });
 
-    about.addEventListener('click',() => {
+    about.addEventListener('click', () => {
         controlPage();
         about.classList.add('active');
         about_section.style.display = "block";
         what_doing_section.style.display = "block";
-    })
-})
+        if (window.innerWidth <= 870 && window.innerWidth >= 561) {
+            document.body.style.height = "1800px";
+            container.style.marginTop = "-1020px";
+            console.log("this is second working...")
+        }else if (window.innerWidth <= 560) {
+            document.body.style.height = "1720px";
+            container.style.marginTop = "-950px";
+            console.log("this is first working...")
+        };
+    });
+});
